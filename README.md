f05699c38e709c8ca3a7ee9e8df6ab43e70c970a
levo <zhangtao13140725@163.com>
2017-04-12 11:32:02 +0800

    packages for  ATOM

---
+++ README.md
@@ -0,0 +1,59 @@
+# Atom packages
+ A cool list of web development packages for Atom.
+
+### Usage
+
+- Download and extract atom-packages.sh.
+- After that run `./atom-packages.sh`
+- Restart your Atom editor and you should have all the cool packages.
+
+###packages list
+- advanced-new-file     打开文件或新建文件，同时支持路径补全
+- atom-beautify         代码格式化 ctr+alt+b
+- atom-minify
+- atom-ternjs    ES5, ES6, ES7, Node.js, jQuery, Angular 等等 js 代码自动补全
+- atomic-chrome   
+- autocomplete-modules    模块名自动补全
+- autocomplete-paths    路径自动补全
+- autocomplete-xml      xml自动补全
+- autoprefixer          浏览器前缀 需要快捷键 也可用gulp解决
+- busy-signal
+- color-picker         取色器  cmd＋shift＋c
+- emmet                Zen coding
+- file-icons           显示文件类型对应的图标
+- hyperclick           跳转到变量定义文件，需要配合 js-hyperclick 使用
+- intentions
+- javascript-snippets  js代码片段
+- js-hyperclick        跳转到变量定义文件，需要配合 hyperclick 使用
+- jshint               语法效验
+- language-babellanguage-babel  ES2016、ESNext、JSX 语法扩展
+- language-vue         vuejs语法高亮
+- language-vue-component   vuejs组件
+- less-autocompile     less编译  压缩min.css  用ctrl+shift+m 覆盖压缩min.css  用com+shift+m
+- linter               基本语法检查框架
+- linter-jshint        基于 jshint 的 JavaScript 语法检查插件
+- linter-ui-default
+- linter-xmllint        xml语法效验
+- markdown-preview-plus   markdown 实时预览
+- minimap   源码预览图
+- minimap-autohide 地图自动隐藏
+- minimap-codeglance
+- minimap-find-and-replace
+- minimap-highlight-selected   源码预览图中高亮选中
+- minimap-linter
+- quick-highlight  选择高亮
+- run-in-browser   浏览器运行 ctrl+alt+r  
+- script  运行js  cmd + i
+- vue-autocomplete  vue语法自动补全
+- xml-formatter     xml语法格式化
+
+      切换窗口cmd+t
+      在atom中打开控制台cmd+alt+i
+      打开终端comm+`
+      注释补全  输入/两个星号后 按tab
+      comm+r  搜索函数等
+      通过 Cmd-Alt-O快速的打开文件或新建文件，同时支持路径补全。
+      通过 Cmd-shift-O快速的在本窗口中打开项目
+      通过 Cmd-O快速的在新窗口中打开项目
+
+:smiley: :coffee:
---
+++ atom-packages.sh
@@ -0,0 +1,147 @@
+# A cool list of web development packages for Atom.
+# Before you start running this shell script, I assume that you already have
+# atom editor installed in your computer.
+# For more information about how to install atom:
+# https://github.com/atom/atom#installing
+
+
+# Installing Packages for Atom .
+# Drupal Packages:
+
+# language-drupal (http://drupal.org) syntax highlighting and snippets for Atom.
+#apm install language-drupal
+
+# drupal Drupal snippets, filetypes and hotkeys
+#apm install drupal
+
+# drupal-info-file Drupal info file syntax
+#apm install drupal-info-file
+
+#fancy-new-file Create files and directories by typing a relative path
+#apm install fancy-new-file
+
+apm install advanced-new-file
+# atom-beautify Beautify HTML, CSS, JavaScript, PHP, Python, Ruby, Java, C, C++,
+# C#, Objective-C, CoffeeScript, TypeScript, and SQL in Atom
+apm install atom-beautify
+
+apm install atom-ternjs
+
+apm install atom-minify
+
+apm install atomic-chrome
+
+apm install autocomplete-modules
+
+apm install autocomplete-paths
+
+apm install autocomplete-xml
+
+apm install autoprefixer
+
+apm install busy-signal
+
+apm install color-picker
+
+#emmet The essential tool for web developers
+apm install emmet
+
+# file-icons - Assign file extension icons and colours for improved visual
+# grepping
+apm install file-icons
+
+apm install hyperclick
+
+apm install intentions
+
+# javascript-snippets - JavaScript & NodeJS Snippets for Atom
+apm install javascript-snippets
+
+apm install js-hyperclick
+# Atom Web Development
+
+
+
+# jshint Validate JavaScript with JSHint. In realtime or on save. Supports JSX
+apm install jshint
+
+apm install language-babellanguage-babel
+
+amp install language-vue
+
+amp install language-vue-component
+
+amp install less-autocompile
+
+# linter Validates your code using lintersatom-beautify
+apm install linter
+
+amp install linter-jshint
+amp install linter-ui-default
+amp install linter-xmllint
+
+amp install markdown-preview-plus
+
+ # minimap
+amp install minimap
+amp install minimap-autohide
+amp install minimap-codeglance
+amp install minimap-find-and-replace
+amp install minimap-highlight-selected
+amp install minimap-linter
+
+amp install quick-highlight
+amp install run-in-browser
+amp install script
+amp install vue-autocomplete
+amp install xml-formatter
+
+
+
+
+ # change-case Change the case of selected text using node-change-case
+  # apm install change-case
+
+
+# atom-alignment A simple key-binding for aligning multi-line and multiple
+# selections in Atom (Based on the sublime text plugin)
+ ##apm install atom-alignment
+
+
+# atom-html-preview A live preview tool for Atom Editor.
+# apm install atom-html-preview
+
+# bootstrap-3-snippetset A 'Twitter Bootstrap 3' snippetset.
+# apm install bootstrap-3-snippetset
+
+
+# term2 - Terminal build in Atom
+#apm install term2
+
+# Temporary term2 bug fix.
+
+# cd ~/.atom/packages/term2/node_modules
+# rm -rf atom-term.js
+# git clone https://github.com/mmckegg/atom-term.js.git atom-term.js
+# cd atom-term.js
+# git checkout patch-1
+# cd ~/
+
+# autocomplete-plus - Display possible completions in the editor while typing
+#apm install autocomplete-plus
+
+
+# atom-css-comb - CSScomb is a coding style formatter for CSS (LESS|SASS|etc).
+# You can easily write your own configuration to make your style sheets
+# beautiful and consistent.
+#apm install atom-css-comb
+
+# git-plus - Do git things without the terminal
+#apm install git-plus
+
+# sftp-deployment - A package which allow you to upload and download files with
+# FTP/SFTP protocol
+#apm install sftp-deployment
+
+
+# TODO linter-phpcs with phpcs configuration.
